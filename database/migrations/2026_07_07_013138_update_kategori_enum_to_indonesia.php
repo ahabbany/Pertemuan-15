000abe8c8e0b6e5abc99a04610f5bf4cff5d0cbf
Ahabbany <ahabbany@gmail.com>
2026-07-07 10:33:20 +0700

    perbaikan ulang

--- database/migrations/2026_07_07_013138_update_kategori_enum_to_indonesia.php
+++ database/migrations/2026_07_07_013138_update_kategori_enum_to_indonesia.php
@@ -7,11 +7,23 @@ return new class extends Migration
 {
     public function up()
     {
+        DB::statement("UPDATE buku SET kategori = 'Pemrograman' WHERE kategori = 'Programming'");
+        DB::statement("UPDATE buku SET kategori = 'Basis Data' WHERE kategori = 'Database'");
+        DB::statement("UPDATE buku SET kategori = 'Desain Web' WHERE kategori = 'Web Design'");
+        DB::statement("UPDATE buku SET kategori = 'Jaringan' WHERE kategori = 'Networking'");
+        DB::statement("UPDATE buku SET kategori = 'Ilmu Data' WHERE kategori = 'Data Science'");
+
         DB::statement("ALTER TABLE buku MODIFY COLUMN kategori ENUM('Pemrograman', 'Basis Data', 'Desain Web', 'Jaringan', 'Ilmu Data') NOT NULL");
     }
 
     public function down()
     {
+        DB::statement("UPDATE buku SET kategori = 'Programming' WHERE kategori = 'Pemrograman'");
+        DB::statement("UPDATE buku SET kategori = 'Database' WHERE kategori = 'Basis Data'");
+        DB::statement("UPDATE buku SET kategori = 'Web Design' WHERE kategori = 'Desain Web'");
+        DB::statement("UPDATE buku SET kategori = 'Networking' WHERE kategori = 'Jaringan'");
+        DB::statement("UPDATE buku SET kategori = 'Data Science' WHERE kategori = 'Ilmu Data'");
+
         DB::statement("ALTER TABLE buku MODIFY COLUMN kategori ENUM('Programming', 'Database', 'Web Design', 'Networking', 'Data Science') NOT NULL");
     }
 };
